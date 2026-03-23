26d2dcb7e3900f08c894c69a5d52df71536cb763
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-03-23 21:29:49 +0530

    feat : implement app logic in JS

---
+++ Calculator-App/script.js
@@ -0,0 +1,16 @@
+let string = "";
+let buttons = document.querySelectorAll(".calc-button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (event.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else {
+      console.log(event.target);
+      string = string + event.target.innerHTML;
+      console.log(string);
+
+      document.querySelector("input").value = string;
+    }
+  });
+});
