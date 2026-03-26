fabfa0462902aa426e4e9e4e6d2b49c88b5cc6f3
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-03-26 23:52:40 +0530

    feat : initialise DOM elements for core JS logic

---
+++ Password-Generator/script.js
@@ -0,0 +1,6 @@
+const range = document.getElementById("passwordRange");
+const number = document.getElementById("passwordLength");
+
+const includeUppercase = document.getElementById("uppercase");
+const includeDigits = document.getElementById("digits");
+const includeSymbols = document.getElementById("symbols");
