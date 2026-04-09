c59dc1870219117da8e5d5850687449a0a283fcf
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-04-10 00:00:02 +0530

    feat : select elements in DOM

---
+++ CurrenSync/script.js
@@ -0,0 +1,10 @@
+const URL =
+  "http://v6.exchangerate-api.com/v6/8085db0e7f5923c434961189/latest/USD";
+
+const dropdown = document.querySelectorAll(".dropdown select");
+const button = document.querySelector("button");
+const amount = document.querySelector("form input");
+const displayText = document.querySelector(".display");
+
+const currency1 = document.querySelector(".from select");
+const currency2 = document.querySelector(".to select");
