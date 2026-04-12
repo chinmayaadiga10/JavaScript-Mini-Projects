4393528ca8eacb65c779269de78ea951f8509853
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-04-12 14:05:27 +0530

    feat : main handler function setup

--- WordGloss-Dictionary/script.js
+++ WordGloss-Dictionary/script.js
@@ -10,3 +10,33 @@ const loader = document.querySelector(".loader");
 const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/%3Cword%3E";
 
 searchButton.addEventListener("click", mainHandler);
+
+async function mainHandler(event) {
+  event.preventDefault();
+  console.log("button was clicked");
+  const searchWord = word.value.trim().toLowerCase();
+  console.log(searchWord);
+  if (searchWord === "") {
+    alert("please enter valid search word");
+    return;
+  }
+  showLoader();
+  solutionSection.classList.add("hidden");
+
+  try {
+    let resultAPI = await fetchWord(searchWord);
+
+    hideLoader();
+    solutionSection.classList.remove("hidden");
+
+    extractData(resultAPI);
+  } catch (error) {
+    hideLoader();
+
+    if (error.message === "WORD NOT FOUND") {
+      showError(`No results found for "${searchWord}"`);
+    } else {
+      showError("Something went wrong. Try again later");
+    }
+  }
+}
