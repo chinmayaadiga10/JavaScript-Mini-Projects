8df477ecb6d299aa9558a5a2dcd21d1ea413f294
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-04-14 15:52:03 +0530

    feat : event listeners to control the bird

--- FlappyBird-Game/script.js
+++ FlappyBird-Game/script.js
@@ -12,3 +12,32 @@ let score_val = document.querySelector(".score-value");
 
 let message = document.querySelector(".message");
 let score_title = document.querySelector(".score-title");
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "ArrowUp" && game_state === "Play") {
+    image.src = "Resources/Bird-2.png";
+    bird_dy = -7.6;
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key == "ArrowUp") {
+    image.src = "Resources/Bird.png";
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key == "Enter" && game_state != "Play") {
+    document.querySelectorAll(".pipe").forEach((el) => {
+      el.remove();
+    });
+    image.style.display = "block";
+    bird.style.top = "40vh";
+    game_state = "Play";
+    message.innerHTML = "";
+    score_title.innerHTML = "Score : ";
+    score_val.innerHTML = "0";
+    message.classList.remove("message-style");
+    play();
+  }
+});
